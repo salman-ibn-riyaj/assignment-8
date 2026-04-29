f13f2698858ac39dc98fda3122eae525e23fe010
salman <salmanibnriyaj@gmail.com>
2026-04-30 01:25:42 +0600

    booking form added

--- src/app/all-animals/[id]/page.jsx
+++ src/app/all-animals/[id]/page.jsx
@@ -1,3 +1,5 @@
+
+import BookingForm from "@/components/BookingForm";
 import { Card } from "@heroui/react";
 import Image from "next/image";
 
@@ -13,34 +15,43 @@ const DetailsPage = async ({ params }) => {
   const cow = cows.find((c) => c.id == id);
   console.log(cow);
 
-  return (
-    <Card className="my-12">
-      <div className="relative w-full aspect-square">
-        <Image
-          alt={cow.name}
-          fill
-          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-          className="absolute pointer-events-none aspect-square rounded-2xl object-cover select-none"
-          loading="lazy"
-          src={cow.image}
-        />
-      </div>
 
-      <Card.Header>
-        <Card.Title className="text-xl font-bold">{cow.name}</Card.Title>
-        <Card.Description>{cow.category}</Card.Description>
-      </Card.Header>
-      <div className="flex justify-between">
-        <span className="font-bold">Weight: {cow.weight}</span>
-        <span className="font-bold">Age: {cow.age}</span>
-      </div>
+
+  return (
+    <>
+      <Card className="my-12">
+        <div className="relative w-full aspect-square">
+          <Image
+            alt={cow.name}
+            fill
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+            className="absolute pointer-events-none aspect-square rounded-2xl object-cover select-none"
+            loading="lazy"
+            src={cow.image}
+          />
+        </div>
+
+        <Card.Header>
+          <Card.Title className="text-xl font-bold">{cow.name}</Card.Title>
+          <Card.Description>{cow.category}</Card.Description>
+        </Card.Header>
+        <div className="flex justify-between">
+          <span className="font-bold">Weight: {cow.weight}</span>
+          <span className="font-bold">Age: {cow.age}</span>
+        </div>
+        <div>
+          <h2 className="text-xl font-bold">Price: ৳ {cow.price}</h2>
+        </div>
+        <div className="font-bold">Location: {cow.location}</div>
+        <div className="font-bold">Category: {cow.category}</div>
+        <div className="font-bold">Description: {cow.description}</div>
+      </Card>
+
       <div>
-        <h2 className="text-xl font-bold">Price: ৳ {cow.price}</h2>
+        <h2 className="text-2xl font-bold mb-4">Booking Form:</h2>
+        <BookingForm></BookingForm>
       </div>
-      <div className="font-bold">Location: {cow.location}</div>
-      <div className="font-bold">Category: {cow.category}</div>
-      <div className="font-bold">Description: {cow.description}</div>
-    </Card>
+    </>
   );
 };
 
---
+++ src/components/BookingForm.jsx
@@ -0,0 +1,51 @@
+"use client";
+import { Check } from "@gravity-ui/icons";
+import {
+  Button,
+  Description,
+  FieldError,
+  Form,
+  Input,
+  Label,
+  TextField,
+} from "@heroui/react";
+import { Toast, toast } from "@heroui/react";
+
+const BookingForm = () => {
+  return (
+    <Form className="flex w-96 flex-col gap-4 mb-10">
+      <TextField isRequired name="name" type="text">
+        <Label>Name</Label>
+        <Input placeholder="Enter your name" />
+        <FieldError />
+      </TextField>
+      <TextField isRequired name="email" type="email">
+        <Label>Email</Label>
+        <Input placeholder="Enter your email" />
+        <FieldError />
+      </TextField>
+      <TextField isRequired minLength={8} name="phone" type="text">
+        <Label>Phone</Label>
+        <Input placeholder="Enter your Phone number" />
+        <FieldError />
+      </TextField>
+      <TextField isRequired minLength={8} name="address" type="text">
+        <Label>Phone</Label>
+        <Input placeholder="Enter your address" />
+        <FieldError />
+      </TextField>
+      <div className="flex gap-2">
+        <Toast.Provider />
+        <Button onPress={() => toast("Booking Success")} type="submit">
+          <Check />
+          Submit
+        </Button>
+        <Button type="reset" variant="secondary">
+          Reset
+        </Button>
+      </div>
+    </Form>
+  );
+};
+
+export default BookingForm;
